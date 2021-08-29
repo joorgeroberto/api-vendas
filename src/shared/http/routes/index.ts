26631ecf695798369b798c4a4b78754247130793
Jorge de Carvalho <jorgercj@dcomp.ufs.br>
2021-08-29 18:15:35 -0300

    Creating sessions Controller and route

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import productsRouter from '@modules/products/routes/products.routes';
 import usersRouter from '@modules/users/routes/users.routes';
+import sessionsRouter from '@modules/users/routes/sessions.routes';
 
 const routes = Router();
 
@@ -8,6 +9,8 @@ const routes = Router();
 routes.use('/products', productsRouter);
 
 routes.use('/users', usersRouter);
+
+routes.use('/sessions', sessionsRouter);
 
 routes.get('/', (request, response) => {
   return response.json({ message: 'Agora vai!' });
